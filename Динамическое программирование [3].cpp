8dfea83d0cee607ed1f9758598a2d95c36c92863
Yusuf190507 <deryabashkochesi963@gmail.com>
2026-07-09 20:18:45 +0300

    Create Динамическое программирование [3].cpp

---
+++ Динамическое программирование [3].cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+//нет слов...
+
+int main() {
+    int n;
+    std::cin >> n;
+    for (int i = n; i > 0; --i) {
+        std::cout << i << " ";
+    }
+    std::cout << "\n";
+}
